0ed9088b97870a75b8e6f3ca22bc00eae5233370
Raymon <rbasnyat666@gmail.com>
2023-01-26 00:46:50 +0545

    add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "../dist/output.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
---
+++ src/pages/EmployeVerify.tsx
@@ -0,0 +1,10 @@
+import SearchBar from "../components/SearchBar";
+
+const EmployeeVerify = () => {
+  return (
+    <div className="h-full flex justify-center items-center">
+      <SearchBar />
+    </div>
+  );
+};
+export default EmployeeVerify;
